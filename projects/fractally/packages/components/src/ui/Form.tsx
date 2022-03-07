87506621ac8ff19318dd203e05280b10348a557f
sparkplug0025 <sparkplug0025@gmail.com>
2022-03-07 00:44:56 -0500

    Isolate StyledForm

--- projects/fractally/packages/components/src/ui/Form.tsx
+++ projects/fractally/packages/components/src/ui/Form.tsx
@@ -1,6 +1,4 @@
 import React, { HTMLProps } from "react";
-import styled from "styled-components";
-import tw from "twin.macro";
 
 import "../styles/inputs.css";
 
@@ -108,22 +106,6 @@ export const LabeledSet: React.FC<{
     );
 };
 
-const StyledForm = styled.main.attrs({
-    className: "flex flex-col h-screen justify-center items-center bg-gray-100",
-})`
-    & {
-        form {
-            ${tw`bg-white text-center rounded py-8 px-5 shadow max-w-xs`}
-        }
-        input {
-            ${tw`border-gray-300 mb-4 w-full border-solid border rounded py-2 px-4`}
-        }
-        button {
-            ${tw`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
-        }
-    }
-`;
-
 export const Form = {
     Label,
     Input,
@@ -132,7 +114,6 @@ export const Form = {
     LabeledSet,
     FileInput,
     Checkbox,
-    StyledForm,
 };
 
 export default Form;
