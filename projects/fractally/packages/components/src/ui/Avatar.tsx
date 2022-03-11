183b5a1de101737226cfcfdaca5a814bae91947d
Brandon Fancher <brandonfancher@gmail.com>
2022-03-11 15:58:14 -0500

    Fix Safari SVG clipPath rendering issues.

--- projects/fractally/packages/components/src/ui/Avatar.tsx
+++ projects/fractally/packages/components/src/ui/Avatar.tsx
@@ -22,17 +22,14 @@ export interface AvatarProps {
 }
 
 /**
- * 🚧 **TODO**: This component uses a hexagonal SVG with rounded corners to mask the avatar image. Currently, however, this SVG is
- * loaded alongside each instance of this component. There could be 20+ avatars on a single page, which means 20+ duplicate
- * SVGs rendered. For that reason, when we start to use this component, we should consider loading the SVG once on each
- * page (it may be used for icons too) and should ensure it's there and available before dependent components use it.
+ * ℹ️ This component uses a hexagonal SVG with rounded corners to mask the avatar image. (See Avatar.tsx for the SVG implementation).
+ * This SVG is invisible (takes up no space) and should be loaded once on the page so it can be referenced by as many instances of Avatar
+ * require it. In Storybook, this is done in `.storybook/preview-body.html`.
  *
  * 🚧 **TODO**: Additionally, the `FaUserCircle` icon we're using has a border. We add a div to cover the border, but it's imperfect and
  * unnecessary. We should just create and use a custom SVG for this and get rid of the extra div.
  *
- * 🐜 **BUG**: Because the SVG hex mask is currently loaded alongside each instance, it may not be there when referenced (race condition).
- * (If you don't see the hex mask on the Storybook Docs tab, that would why. Just refresh the page.) When we address the first TODO
- * above, this should be resolved.
+ * 🚧 **TODO**: Thomas has a better version of the hexagon SVG waiting for us to implement (this one is tilted barely).
  */
 export const Avatar = ({
     name,
@@ -46,8 +43,10 @@ export const Avatar = ({
     const hexStyle = shape === "hex" ? { clipPath: "url(#hex-mask)" } : {};
     return (
         <>
-            {shape === "hex" && <HexMaskSVG />}
-            <div className={`${sizeClass} ${className}`} style={hexStyle}>
+            <div
+                className={`${sizeClass} ${className} overflow-hidden`}
+                style={hexStyle}
+            >
                 <div className={`${sizeClass} ${roundClass} overflow-hidden`}>
                     {avatarUrl ? (
                         <img
@@ -74,12 +73,12 @@ export const Avatar = ({
 const HexMaskSVG = () => (
     <svg width="0" height="0">
         <defs>
-            <clipPath id="hex-mask" clipPathUnits="objectBoundingBox">
-                <path
-                    id="curve"
-                    transform="scale(0.019999989318853, 0.022522370814944)"
-                    d="M0.549499 24.2509C-0.183167 22.9819 -0.183166 21.4184 0.5495 20.1494L10.9987 2.05076C11.7314 0.781744 13.0854 0 14.5508 0H35.4492C36.9146 0 38.2686 0.781745 39.0013 2.05076L49.4505 20.1494C50.1832 21.4184 50.1832 22.9819 49.4505 24.2509L39.0013 42.3495C38.2686 43.6185 36.9146 44.4003 35.4492 44.4003H14.5508C13.0854 44.4003 11.7314 43.6185 10.9987 42.3495L0.549499 24.2509Z"
-                ></path>
+            <clipPath
+                id="hex-mask"
+                clipPathUnits="objectBoundingBox"
+                transform="scale(0.019999989318853, 0.022522370814944)"
+            >
+                <path d="M0.549499 24.2509C-0.183167 22.9819 -0.183166 21.4184 0.5495 20.1494L10.9987 2.05076C11.7314 0.781744 13.0854 0 14.5508 0H35.4492C36.9146 0 38.2686 0.781745 39.0013 2.05076L49.4505 20.1494C50.1832 21.4184 50.1832 22.9819 49.4505 24.2509L39.0013 42.3495C38.2686 43.6185 36.9146 44.4003 35.4492 44.4003H14.5508C13.0854 44.4003 11.7314 43.6185 10.9987 42.3495L0.549499 24.2509Z"></path>
             </clipPath>
         </defs>
     </svg>
