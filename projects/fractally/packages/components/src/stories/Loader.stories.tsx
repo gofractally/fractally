ad8d2a75060b12bb2be24435a85fc6115c08d317
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-03-14 09:26:54 -0400

    Add splash option to Loader

--- projects/fractally/packages/components/src/stories/Loader.stories.tsx
+++ projects/fractally/packages/components/src/stories/Loader.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-
 import { Loader } from "../ui/Loader";
 
 export default {
@@ -10,3 +9,24 @@ export default {
 const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />;
 
 export const Default = Template.bind({});
+
+const SplashTemplate: ComponentStory<typeof Loader> = (args) => {
+    return (
+        <div className="flex flex-col">
+            <h3>Example Page</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <Loader {...args} />
+        </div>
+    );
+};
+
+export const Splash = SplashTemplate.bind({});
+Splash.args = { splash: true };
--- projects/fractally/packages/components/src/ui/Loader.tsx
+++ projects/fractally/packages/components/src/ui/Loader.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import LoadingIcon from "./icons/LoadingIcon";
 
-export const Loader = ({ size = 32 }: { size?: number }) => (
-    <div className="loader w-full h-full flex justify-center items-center">
-        <LoadingIcon size={size} />
-    </div>
-);
+export const Loader = ({
+    size = 32,
+    splash = false,
+}: {
+    size?: number;
+    splash: boolean;
+}) => {
+    if (splash) {
+        return (
+            <div
+                aria-modal="true"
+                className="bg-slate-500/50 backdrop-blur-sm overflow-y-auto overflow-x-hidden fixed h-full right-0 left-0 z-1 justify-center items-center h-modal flex"
+            >
+                <div className="relative">
+                    <LoadingIcon size={size} />
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="loader w-full h-full flex justify-center items-center">
+            <LoadingIcon size={size} />
+        </div>
+    );
+};
 
 export default Loader;
