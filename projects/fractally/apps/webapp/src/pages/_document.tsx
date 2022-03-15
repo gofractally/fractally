23ba084b6f26e63470a99d1954dbd25024c89bb4
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-03-15 14:59:50 -0400

    Fix eslint issue

--- projects/fractally/apps/webapp/src/pages/_document.tsx
+++ projects/fractally/apps/webapp/src/pages/_document.tsx
@@ -1,22 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Raleway&display=optional"
-                        rel="stylesheet"
-                    />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
-}
+const MyDocument = () => {
+    return (
+        <Html>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Raleway&display=optional"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
 
 export default MyDocument;
