098816352ba3fe72780c3dffd0ee211150579a1e
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-03-17 16:07:33 -0400

    Add examples of Modals to select From and To

--- projects/fractally/packages/components/src/ui/SendDialog.tsx
+++ projects/fractally/packages/components/src/ui/SendDialog.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "../ui/Card";
 import { Avatar } from "../ui/Avatar";
 import Button from "../ui/Button";
+import { Modal } from "../ui/Modal";
 
 import "../styles/send-dialog.css";
+
 
 interface SDFieldProps {
     children: React.ReactNode[];
     avatarUrl?: string;
-    onOpen?: () => void;
+    onClick?: () => void;
 }
 
-const SDField = ({ children, avatarUrl, onOpen }: SDFieldProps) => {
+const SDField = ({ children, avatarUrl, onClick }: SDFieldProps) => {
     return (
-        <div className="SDField flex gap-1 border px-2 py-1 mb-4">
+        <div
+            className={`SDField flex gap-1 border px-2 py-1 mb-4 ${ onClick && "cursor-pointer" }`}
+            onClick={onClick}
+        >
             {avatarUrl && (
                 <div className="flex-none m-1">
                     <Avatar
@@ -29,7 +34,7 @@ const SDField = ({ children, avatarUrl, onOpen }: SDFieldProps) => {
                     {children}
                 </div>
             </div>
-            {onOpen && (
+            {onClick && (
                 <div className="flex-none content-center mx-2 my-auto text-slate-400">
                     &gt;
                 </div>
@@ -43,6 +48,8 @@ export interface SendDialogProps {
 }
 
 const SendDialog = ({ className }: SendDialogProps) => {
+    const [isFromModalOpen, setFromModalOpen] = useState(false);
+    const [isToModalOpen, setToModalOpen] = useState(false);
     return (
         <div className="bg-gray-100 w-full h-full p-4 space-y-4">
             <Card>
@@ -50,11 +57,11 @@ const SendDialog = ({ className }: SendDialogProps) => {
                     <h3 className="mb-6 mx-auto font-bold text-center">Send</h3>
                     <SDField
                         avatarUrl="https://randomuser.me/api/portraits/women/44.jpg"
-                        onOpen={() => console.log("onOpen 'From'")}
+                        onClick={() => setFromModalOpen(true)}
                     >
                         <label className="text-slate-400">From</label>
                         <div className="font-bold">PRIME</div>
-                        <div>Total: 2,800 / B0.1034011</div>
+                        <div>Total: 2,800 / {"\u20BF"}0.1034011</div>
                     </SDField>
                     <SDField>
                         <label className="text-slate-400">Amount</label>
@@ -62,7 +69,7 @@ const SendDialog = ({ className }: SendDialogProps) => {
                     </SDField>
                     <SDField
                         avatarUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                        onOpen={() => console.log("onOpen 'To'")}
+                        onClick={() => setToModalOpen(true)}
                     >
                         <label className="text-slate-400">To</label>
                         <div>Hal McGovern</div>
@@ -77,6 +84,44 @@ const SendDialog = ({ className }: SendDialogProps) => {
                     </div>
                 </div>
             </Card>
+
+            {/* Modal Dialogs Examples */}
+            <Modal
+                shouldCloseOnEsc
+                shouldCloseOnOverlayClick
+                ariaHideApp={false}
+                isOpen={isFromModalOpen}
+                onRequestClose={() => setFromModalOpen(false)}
+                title="Select your fractal and currency"
+            >
+                <div className="space-y-3">
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+                        consectetur debitis omnis blanditiis cum at laborum doloremque,
+                        nam rem nesciunt error nemo voluptatibus quos quisquam non
+                        recusandae asperiores, sed quas.
+                    </div>
+                    <div>Press [ESC] or click in the Overlay to close it</div>
+                </div>
+            </Modal>
+            <Modal
+                shouldCloseOnEsc
+                shouldCloseOnOverlayClick
+                ariaHideApp={false}
+                isOpen={isToModalOpen}
+                onRequestClose={() => setToModalOpen(false)}
+                title="Select recipient"
+            >
+                <div className="space-y-3">
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+                        consectetur debitis omnis blanditiis cum at laborum doloremque,
+                        nam rem nesciunt error nemo voluptatibus quos quisquam non
+                        recusandae asperiores, sed quas.
+                    </div>
+                    <div>Press [ESC] or click in the Overlay to close it</div>
+                </div>
+            </Modal>
         </div>
     );
 };
