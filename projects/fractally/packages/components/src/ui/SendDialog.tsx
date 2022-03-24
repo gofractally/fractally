555d6aa2ce6456802e624ef4edb4d0701b524a4d
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-03-24 17:18:44 -0400

    Refactor SendDialog storybook to use mdx syntax

--- projects/fractally/packages/components/src/ui/SendDialog.tsx
+++ projects/fractally/packages/components/src/ui/SendDialog.tsx
@@ -1,50 +1,13 @@
 import React, { useState } from "react";
-import { Card } from "../ui/Card";
-import { Avatar } from "../ui/Avatar";
-import Button from "../ui/Button";
-import { Modal } from "../ui/Modal";
-import ConfirmSendDialog from "../ui/ConfirmSendDialog";
+import { Card } from "./Card";
+import Button from "./Button";
+import { Modal } from "./Modal";
+import SendDialogField from "./SendDialogField";
+import ConfirmSendDialog from "./ConfirmSendDialog";
 
 import "../styles/send-dialog.css";
 
-interface SDFieldProps {
-    children: React.ReactNode[];
-    avatar?: {
-        url?: string;
-        name?: string;
-    };
-    onClick?: () => void;
-}
 
-const SendDialogField = ({ children, avatar, onClick }: SDFieldProps) => {
-    return (
-        <div
-            className={`SendDialogField flex gap-1 border px-2 py-1 mb-4 ${onClick && "cursor-pointer"}`}
-            onClick={onClick}
-        >
-            {avatar && (
-                <div className="flex-none m-1">
-                    <Avatar
-                        avatarUrl={avatar.url}
-                        name="Rey"
-                        size="lg"
-                        shape="hex"
-                    />
-                </div>
-            )}
-            <div className="flex-auto content-center mx-1 my-auto">
-                <div className={`contentLen-${children.length} text-sm`}>
-                    {children}
-                </div>
-            </div>
-            {onClick && (
-                <div className="flex-none content-center mx-2 my-auto text-slate-400">
-                    &gt;
-                </div>
-            )}
-        </div>
-    );
-};
 
 const SendDialog = () => {
     const [isFromModalOpen, setFromModalOpen] = useState(false);
